1ed4f572514dad83ddff6cae9711d26a83a93a9f
hamz0640 <126711289+hamz0640@users.noreply.github.com>
2026-04-08 20:58:17 +0200

    Moverecord

---
+++ Programmeringseksamens projekt/MoveRecord.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmeringseksamens_projekt
+{
+    public class MoveRecord
+    {
+        public Move Move { get; set; }
+        public Piece CapturedPiece { get; set; }
+        public bool PieceHadMoved { get; set; }
+        public (int, int)? PreviousEnPassantTarget { get; set; }
+        public bool PreviousWhiteCanCastleKingside { get; set; }
+        public bool PreviousWhiteCanCastleQueenside { get; set; }
+        public bool PreviousBlackCanCastleKingside { get; set; }
+        public bool PreviousBlackCanCastleQueenside { get; set; }
+
+        public MoveRecord(Move move, Piece capturedPiece, bool pieceHadMoved,
+            (int, int)? previousEnPassantTarget,
+            bool previousWhiteCanCastleKingside,
+            bool previousWhiteCanCastleQueenside,
+            bool previousBlackCanCastleKingside,
+            bool previousBlackCanCastleQueenside)
+        {
+            Move = move;
+            CapturedPiece = capturedPiece;
+            PieceHadMoved = pieceHadMoved;
+            PreviousEnPassantTarget = previousEnPassantTarget;
+            PreviousWhiteCanCastleKingside = previousWhiteCanCastleKingside;
+            PreviousWhiteCanCastleQueenside = previousWhiteCanCastleQueenside;
+            PreviousBlackCanCastleKingside = previousBlackCanCastleKingside;
+            PreviousBlackCanCastleQueenside = previousBlackCanCastleQueenside;
+        }
+    }
+}
