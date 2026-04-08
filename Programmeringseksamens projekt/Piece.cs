077b3c600c8f77572dbb162dd26f196b35591289
hamz0640 <126711289+hamz0640@users.noreply.github.com>
2026-04-08 20:59:14 +0200

    Piece

---
+++ Programmeringseksamens projekt/Piece.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Programmeringseksamens_projekt.Enums;
+
+namespace Programmeringseksamens_projekt
+{
+    public abstract class Piece
+    {
+        public PieceColor Color { get; set; }
+        public PieceType Type { get; set; }
+        public (int row, int col) Position { get; set; }
+        public bool HasMoved { get; set; } = false;
+
+        public Piece(PieceColor color, PieceType type, (int, int) position)
+        {
+            Color = color;
+            Type = type;
+            Position = position;
+        }
+
+        public abstract List<Move> GetLegalMoves(Board board);
+    }
+}
